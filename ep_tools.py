ed94447e53e6ba40f98dbee4a401cf6f2f5fee3c
LaurentRavera <laurent.ravera@irap.omp.eu>
2021-05-19 11:09:04 +0200

    Addition of DRE copyright and licence header (fixes #5)

--- ep_tools.py
+++ ep_tools.py
@@ -1,5 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2021-2030 <FirstnameName>, IRAP Toulouse.
+#  This file is part of the ATHENA X-IFU DRE data analysis tools software.
+#
+#  analysis-tools is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  laurent.ravera@irap.omp.eu
+#  ep_tools.py
+#
 
 import os
 import numpy as np
